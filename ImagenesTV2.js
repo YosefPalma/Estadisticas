683fa0a7c58d20d846cfee1c2aa09a365d59a57b
YosefPalma <yosefpapu6@gmail.com>
2026-01-08 14:06:09 -0600

    cambios

---
+++ ImagenesTV2.js
@@ -0,0 +1,32 @@
+// ============================================
+// CONFIGURACIÓN DE IMÁGENES - ARCHIVO SEPARADO
+// ============================================
+// Puedes editar solo este archivo para cambiar las imágenes
+//Cambia los links solamente
+var IMAGENES_CONFIG = [
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 1"
+    },
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 2"
+    },
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 3"
+    },
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 4"
+    },
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 5"
+    },
+    {
+
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 6"
+    }
+];
