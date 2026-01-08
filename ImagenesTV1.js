d525d60b2cc88838aadc3c1947412297d6354d52
YosefPalma <yosefpapu6@gmail.com>
2026-01-08 13:39:45 -0600

    seccionar

---
+++ ImagenesTV1.js
@@ -0,0 +1,33 @@
+// ============================================
+// CONFIGURACIÓN DE IMÁGENES - ARCHIVO SEPARADO
+// ============================================
+
+// Guarda este archivo como "config-imagenes.js"
+// Puedes editar solo este archivo para cambiar las imágenes
+
+var IMAGENES_CONFIG = [
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 1"
+    },
+    {
+        url: "https://image2url.com/r2/bucket3/images/1767896601811-2bd63dca-2af4-452f-bfba-77ad688b6f36.jpeg",
+        title: "Imagen 2"
+    },
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767898221426-f8d8f2d6-ca37-4fe8-8a62-3b96985baab2.jpeg",
+        title: "Imagen 3"
+    },
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767894053593-eb514803-25ec-427c-adf6-a66a93afda77.jpeg",
+        title: "Imagen 4"
+    },
+    {
+        url: "https://image2url.com/r2/bucket3/images/1767896601811-2bd63dca-2af4-452f-bfba-77ad688b6f36.jpeg",
+        title: "Imagen 5"
+    },
+    {
+        url: "https://image2url.com/r2/bucket2/images/1767898221426-f8d8f2d6-ca37-4fe8-8a62-3b96985baab2.jpeg",
+        title: "Imagen 6"
+    }
+];
