005b522ba6036f8dde2d091b380296e53156243c
EduarRomero29 <edromeroc@bepensa.com>
2026-01-09 14:22:09 -0600

    Update ImagenesTV2.js

--- ImagenesTV2.js
+++ ImagenesTV2.js
@@ -5,28 +5,28 @@
 //Cambia los links solamente
 var IMAGENES_CONFIG = [
     {
-        url: "https://image2url.com/r2/bucket1/images/1767903217238-c7bbeb2e-d5e8-4cb7-9014-1d170e84a70a.jpeg",
+        url: "https://image2url.com/r2/bucket1/images/1767989982615-7fc8e19e-2640-4935-a5e7-4792be5cf187.jpeg",
         title: "Imagen 1"
     },
     {
-        url: "https://image2url.com/r2/bucket1/images/1767903254743-cd7b3058-0622-4af9-b1d1-45286aab19ab.jpeg",
+        url: "https://image2url.com/r2/bucket1/images/1767989982615-7fc8e19e-2640-4935-a5e7-4792be5cf187.jpeg",
         title: "Imagen 2"
     },
     {
-        url: "https://image2url.com/r2/bucket1/images/1767903217238-c7bbeb2e-d5e8-4cb7-9014-1d170e84a70a.jpeg",
+        url: "https://image2url.com/r2/bucket1/images/1767989982615-7fc8e19e-2640-4935-a5e7-4792be5cf187.jpeg",
         title: "Imagen 3"
     },
     {
-        url: "https://image2url.com/r2/bucket1/images/1767903254743-cd7b3058-0622-4af9-b1d1-45286aab19ab.jpeg",
+        url: "https://image2url.com/r2/bucket2/images/1767990100672-26bfc895-e21e-456e-9155-9549aefda7b5.jpeg",
         title: "Imagen 4"
     },
     {
-        url: "https://image2url.com/r2/bucket1/images/1767903217238-c7bbeb2e-d5e8-4cb7-9014-1d170e84a70a.jpeg",
+        url: "https://image2url.com/r2/bucket2/images/1767990100672-26bfc895-e21e-456e-9155-9549aefda7b5.jpeg",
         title: "Imagen 5"
     },
     {
 
-        url: "https://image2url.com/r2/bucket1/images/1767903254743-cd7b3058-0622-4af9-b1d1-45286aab19ab.jpeg",
+        url: "https://image2url.com/r2/bucket2/images/1767990100672-26bfc895-e21e-456e-9155-9549aefda7b5.jpeg",
         title: "Imagen 6"
     }
 ];
