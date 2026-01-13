0eb154abe5816436b23cdc57b0c26929fc90e685
YosefPalma <133794941+YosefPalma@users.noreply.github.com>
2026-01-13 15:33:48 -0600

    Update image URL for 'Imagen 6'

--- ImagenesTV2.js
+++ ImagenesTV2.js
@@ -25,7 +25,7 @@ var IMAGENES_CONFIG = [
         title: "Imagen 5"
     },
     {
-        url: "https://image2url.com/r2/default/images/1768326787992-fb62e31c-693e-42ba-ae51-ecdcd6c93765.jpeg",
+        url: "https://image2url.com/r2/default/images/1768339851577-ad1307f1-4f01-4a18-8356-3c009d521fd3.jpeg",
         title: "Imagen 6"
     }
 ];
