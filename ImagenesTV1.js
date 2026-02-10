19b6cd4c9658ba081677570cd06c0c071ff2135a
YosefPalma <133794941+YosefPalma@users.noreply.github.com>
2026-02-10 11:28:17 -0600

    Update ImagenesTV1.js

--- ImagenesTV1.js
+++ ImagenesTV1.js
@@ -29,30 +29,6 @@ var IMAGENES_CONFIG = [
         title: "Imagen 6"
     },
     {
-        url: "https://image2url.com/r2/default/images/1768421418985-1f0982ea-b723-46bc-b866-8d0f6d68793c.jpeg",
-        title: "Imagen 7"
-    },
-    {
-        url: "https://image2url.com/r2/default/images/1768941601384-7f20a59d-546c-4fdd-af95-074768e5338b.png",
-        title: "Imagen 8"
-    },
-    {
-        url: "https://image2url.com/r2/default/images/1768941626343-d843d36f-a76c-4e1f-ae7e-acd156b9166f.png",
-        title: "Imagen 9"
-    },
-    {
-        url: "https://image2url.com/r2/default/images/1768941649607-7d15654c-0c54-433c-aad1-962713e0d241.png",
-        title: "Imagen 10"
-    },
-    {
-        url: "https://image2url.com/r2/default/images/1768941675393-60a809f5-7866-419a-946c-64bcb6845705.png",
-        title: "Imagen 11"
-    },
-    {
-        url: "https://image2url.com/r2/default/images/1770413815856-77a92a9d-1241-4265-bb66-5809df9024e7.jpeg",
-        title: "Imagen 13"
-    },
-    {
         url: "https://image2url.com/r2/default/images/1769188219759-be7b8dd7-3524-4403-b6dc-c26da64efc26.png",
         title: "Imagen 15"
     },
