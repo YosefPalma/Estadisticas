e8aeb8e37af63e01c70453a05f2e81e1eb0de4d7
YosefPalma <133794941+YosefPalma@users.noreply.github.com>
2026-03-06 17:44:43 -0600

    Update image URLs in IMAGENES_CONFIG

--- ImagenesTV1.js
+++ ImagenesTV1.js
@@ -4,12 +4,12 @@
 // Puedes editar solo este archivo para cambiar las imágenes
 //Cambia los links solamente
 var IMAGENES_CONFIG = [
-    { url: "https://image2url.com/r2/default/images/1772639193209-4a820509-8110-442d-9f12-a27d3091ee4b.png", title: "Imagen 1" }, 
-    { url: "https://image2url.com/r2/default/images/1772638759444-9a421008-c674-4338-bb55-11bbd66af86b.png", title: "Imagen 2" }, 
-    { url: "https://image2url.com/r2/default/images/1772639303837-3a1bbe1a-343b-4f80-8afd-7906618f098c.png", title: "Imagen 3" }, 
-    { url: "https://image2url.com/r2/default/images/1772638899434-c9ef574e-c57c-412e-92cb-33301e05cbdd.png", title: "Imagen 4" }, 
-    { url: "https://image2url.com/r2/default/images/1772639347614-836fe618-df23-443f-bf57-200273ccc4f9.png", title: "Imagen 5" }, 
-    { url: "https://image2url.com/r2/default/images/1772639061734-24a1d2a5-8935-452f-8a15-4e61e7987c0f.png", title: "Imagen 6" }, 
+    { url: "https://image2url.com/r2/default/images/1772840380176-5439cf90-9b35-49fe-8a4b-db16f9b8d7b0.png", title: "Imagen 1" }, 
+    { url: "https://image2url.com/r2/default/images/1772840455813-0bbc634a-4ab2-44cd-8fe3-d1c4d28a36a3.png", title: "Imagen 2" }, 
+    { url: "https://image2url.com/r2/default/images/1772840510527-155a6096-bc58-403c-9f9e-54517d1dbc69.png", title: "Imagen 3" }, 
+    { url: "https://image2url.com/r2/default/images/1772840549661-5362835f-a072-4fe2-add2-002774ede5ba.png", title: "Imagen 4" }, 
+    { url: "https://image2url.com/r2/default/images/1772840588861-833d95a5-e2ac-4b68-8019-bdf5da65029f.png", title: "Imagen 5" }, 
+    { url: "https://image2url.com/r2/default/images/1772840663005-c7b7bbae-4ca7-41ef-9348-99b5b3e5697d.png", title: "Imagen 6" }, 
     { url: "https://image2url.com/r2/default/images/1769188219759-be7b8dd7-3524-4403-b6dc-c26da64efc26.png", title: "Imagen 15" }, 
     { url: "https://image2url.com/r2/default/images/1769636991660-5034fcd3-a564-4710-a65a-73e64d024735.png", title: "Imagen 16" },
     { url: "/WhatsApp Video 2026-02-16 at 5.39.35 PM.mp4", title: "Video 1" },
