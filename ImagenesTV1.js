ca465a97aaa20e3be4a3655b607587e9afacd76b
YosefPalma <133794941+YosefPalma@users.noreply.github.com>
2026-03-10 16:54:36 -0600

    Update image URLs in IMAGENES_CONFIG array

--- ImagenesTV1.js
+++ ImagenesTV1.js
@@ -4,11 +4,11 @@
 // Puedes editar solo este archivo para cambiar las imágenes
 //Cambia los links solamente
 var IMAGENES_CONFIG = [
-    { url: "https://image2url.com/r2/default/images/1773150837898-8acb14cf-007d-4af0-8efe-8ff4af0c8636.png", title: "Imagen 1" }, 
+    { url: "https://image2url.com/r2/default/images/1773183148090-4034100c-69ec-4816-8405-15df0f631b84.png", title: "Imagen 1" }, 
     { url: "https://image2url.com/r2/default/images/1773150953314-3aa1ee9e-35a1-4c5e-8b0b-4c38d5fda152.png", title: "Imagen 2" }, 
     { url: "https://image2url.com/r2/default/images/1773151032272-3d18133d-6d97-422c-87c1-fd3a9f6f3ce9.png", title: "Imagen 3" }, 
-    { url: "https://image2url.com/r2/default/images/1773151100515-0deef677-a6de-4d30-86b6-c18060531e1b.png", title: "Imagen 4" }, 
-    { url: "https://image2url.com/r2/default/images/1773151179518-655aedfc-0fcf-48f0-84ea-cbcdc09751c7.png", title: "Imagen 5" }, 
+    { url: "https://image2url.com/r2/default/images/1773183248299-2270a7c2-10e9-474c-93b0-0e23f296d3fc.png", title: "Imagen 4" }, 
+    { url: "https://image2url.com/r2/default/images/1773183205533-6d0ac651-1c0c-4eff-880f-90aa42d5d544.png", title: "Imagen 5" }, 
     { url: "https://image2url.com/r2/default/images/1773151225564-70e9ffd3-bde7-418a-8867-6c8deceffd94.png", title: "Imagen 6" }, 
     { url: "https://image2url.com/r2/default/images/1769188219759-be7b8dd7-3524-4403-b6dc-c26da64efc26.png", title: "Imagen 15" }, 
     { url: "https://image2url.com/r2/default/images/1769636991660-5034fcd3-a564-4710-a65a-73e64d024735.png", title: "Imagen 16" },
