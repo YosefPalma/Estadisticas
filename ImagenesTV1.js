d8e4c8a8418942aff8f3f9439e95b107a8ec2710
YosefPalma <133794941+YosefPalma@users.noreply.github.com>
2026-03-12 17:03:01 -0600

    Update image URLs in IMAGENES_CONFIG array

--- ImagenesTV1.js
+++ ImagenesTV1.js
@@ -4,12 +4,12 @@
 // Puedes editar solo este archivo para cambiar las imágenes
 //Cambia los links solamente
 var IMAGENES_CONFIG = [
-    { url: "https://image2url.com/r2/default/images/1773273359834-f9586579-c653-4d98-877a-0c413cdd4c56.png", title: "Imagen 1" }, 
-    { url: "https://image2url.com/r2/default/images/1773273393700-7075d932-dd69-414e-8ab1-acd2738f720f.png", title: "Imagen 2" }, 
-    { url: "https://image2url.com/r2/default/images/1773273452491-34f1464e-4b8c-4b46-b3b5-033df8f42ac4.png", title: "Imagen 3" }, 
+    { url: "https://image2url.com/r2/default/images/1773356363951-5fa2634d-2840-42dc-ae39-52329cf61fb6.jpeg", title: "Imagen 1" }, 
+    { url: "https://image2url.com/r2/default/images/1773356548724-6672b06c-d0db-4da7-af55-11382e076613.png", title: "Imagen 2" }, 
+    { url: "https://image2url.com/r2/default/images/1773356393614-ec55d1fc-2de0-4707-aad0-0112c1169022.jpeg", title: "Imagen 3" }, 
     { url: "https://image2url.com/r2/default/images/1773273484039-07691606-c479-473c-90ef-9b1f74071fad.png", title: "Imagen 4" }, 
-    { url: "https://image2url.com/r2/default/images/1773273508808-ac2505c8-f5c4-425a-ab8a-52b4bdae29b8.png", title: "Imagen 5" }, 
-    { url: "https://image2url.com/r2/default/images/1773273539292-9aca8546-4296-4429-822f-34219b7b3d40.png", title: "Imagen 6" }, 
+    { url: "https://image2url.com/r2/default/images/1773356427527-e4e70bc8-754f-4ae2-9fb5-0e89f1289c27.jpeg", title: "Imagen 5" }, 
+    { url: "https://image2url.com/r2/default/images/1773356456150-d949bf01-dbe0-4408-a3cc-e04a9c793979.jpeg", title: "Imagen 6" }, 
     { url: "https://image2url.com/r2/default/images/1769188219759-be7b8dd7-3524-4403-b6dc-c26da64efc26.png", title: "Imagen 15" }, 
     { url: "https://image2url.com/r2/default/images/1769636991660-5034fcd3-a564-4710-a65a-73e64d024735.png", title: "Imagen 16" },
     { url: "/WhatsApp Video 2026-02-16 at 5.39.35 PM.mp4", title: "Video 1" },
