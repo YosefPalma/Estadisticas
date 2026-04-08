40948e23a2221f5c36890961e393cb4a6ff24839
YosefPalma <133794941+YosefPalma@users.noreply.github.com>
2026-04-08 09:15:47 -0600

    Update image URLs in IMAGENES_CONFIG array

--- ImagenesTV1.js
+++ ImagenesTV1.js
@@ -4,12 +4,12 @@
 // Puedes editar solo este archivo para cambiar las imágenes
 //Cambia los links solamente
 var IMAGENES_CONFIG = [
-    { url: "https://image2url.com/r2/default/images/1774968969939-a74265fe-5c78-4960-bd94-02881d692518.png", title: "Imagen 1" }, 
-    { url: "https://image2url.com/r2/default/images/1774969059658-920ed855-251e-47f3-a918-781dd1ed4523.png", title: "Imagen 2" }, 
-    { url: "https://image2url.com/r2/default/images/1774969174069-948a81ea-26b4-40b3-83c8-acc88a9cb069.png", title: "Imagen 3" }, 
-    { url: "https://image2url.com/r2/default/images/1774969232965-a66e0ed4-0d2c-4a64-b85a-ca6482ef7c94.png", title: "Imagen 4" }, 
+    { url: "https://image2url.com/r2/default/images/1775660969652-262cff16-b274-4db2-b757-604e4190edbd.png", title: "Imagen 1" }, 
+    { url: "https://image2url.com/r2/default/images/1775661080594-c53a0f6e-e523-400d-a18b-9af491c775e5.png", title: "Imagen 2" }, 
+    { url: "https://image2url.com/r2/default/images/1775661126919-cb9a1fda-3cc9-4b2c-8c24-b4f0513a73fa.png", title: "Imagen 3" }, 
+    { url: "https://image2url.com/r2/default/images/1775661175107-4fde8f56-a166-40ae-8dc0-bb79c4614f41.png", title: "Imagen 4" }, 
     { url: "https://image2url.com/r2/default/images/1774969303638-62ee5e28-42fa-42a0-8f61-f194ac3d0259.png", title: "Imagen 5" }, 
-    { url: "https://image2url.com/r2/default/images/1774969344934-13d73dcf-3a1e-4fa0-ad30-5b7f8b6e7643.png", title: "Imagen 6" }, 
+    { url: "https://image2url.com/r2/default/images/1775661332218-acf58962-4332-485d-9624-cfa741580916.png", title: "Imagen 6" }, 
     { url: "https://image2url.com/r2/default/images/1769188219759-be7b8dd7-3524-4403-b6dc-c26da64efc26.png", title: "Imagen 15" }, 
     { url: "https://image2url.com/r2/default/images/1769636991660-5034fcd3-a564-4710-a65a-73e64d024735.png", title: "Imagen 16" }
 ];
