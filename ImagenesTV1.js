66bcf673ec02fa72a8b5a867613abdeb089ba7e0
YosefPalma <133794941+YosefPalma@users.noreply.github.com>
2026-04-15 12:03:04 -0600

    Update image URL for 'Imagen 1'

--- ImagenesTV1.js
+++ ImagenesTV1.js
@@ -4,7 +4,7 @@
 // Puedes editar solo este archivo para cambiar las imágenes
 //Cambia los links solamente
 var IMAGENES_CONFIG = [
-    { url: "https://image2url.com/r2/default/images/1775835959423-483dad28-5160-4b85-a54e-674429d7a1ca.png", title: "Imagen 1" }, 
+    { url: "https://www.image2url.com/r2/default/images/1776276159616-6efff5f2-9c5e-4701-87fe-c43f9917c105.png", title: "Imagen 1" }, 
     { url: "https://www.image2url.com/r2/default/images/1776272507221-ae9c835e-09f6-4885-a4c1-58fc403e1c74.png", title: "Imagen 2" }, 
     { url: "https://www.image2url.com/r2/default/images/1776272354562-78e64ccd-84ce-4277-9ef7-a86ae5ab72c1.png", title: "Imagen 3" }, 
     { url: "https://www.image2url.com/r2/default/images/1776272552130-67fa0177-f884-4f18-8611-6c43d88b231e.png", title: "Imagen 4" }, 
